2d36db0d2eb1ffa68ebd234f5df51f0230a9728a
dsls <74984844+DavidsantyLS@users.noreply.github.com>
2022-10-17 18:34:05 -0500

    commit
    initial

---
+++ hydrogen.config.ts
@@ -0,0 +1,28 @@
+import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'US',
+    defaultLanguageCode: 'EN',
+    storeDomain:
+      // @ts-ignore
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'gradiwebpruebatecnica.myshopify.com',
+    storefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
+      '97a5e86f582c3f4f49a8a8981f273c0d',
+    privateStorefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
+    storefrontApiVersion: '2022-07',
+    // @ts-ignore
+    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
+  },
+  session: CookieSessionStorage('__session', {
+    path: '/',
+    httpOnly: true,
+    secure: import.meta.env.PROD,
+    sameSite: 'Strict',
+    maxAge: 60 * 60 * 24 * 30,
+  }),
+});
---
+++ src/components/sections/index.ts
@@ -0,0 +1,3 @@
+export {FeaturedCollections} from './FeaturedCollections';
+export {Hero} from './Hero';
+export {Banner} from './banner';
